e10bf13feb29619b134bd389ce38fc57f5011111
Chris Martin <cmartin@palantir.com>
2016-04-23 11:53:17 -0500

    Problem 15

--- problems/src/problem_014.rs
+++ problems/src/problem_014.rs
@@ -1,77 +1,11 @@
-use std::collections::HashMap;
-
-fn next_collatz(n: u64) -> u64{
+fn next_collatz(n: usize) -> usize{
     match n % 2{
         0 => n/2,
         _ => 3 * n + 1,
     }
 }
 
-fn collatz_seq_length(num: u64, mut seq_lens: HashMap<u64,u64>) -> u64{
-    let next_num = next_collatz(num);
-    if seq_lens.contains_key(&next_num){
-        return (1 + seq_lens.get(&next_num).unwrap()) as u64;
-    } else {
-        let seq_len = collatz_seq_length(next_num, seq_lens.clone());
-        seq_lens.insert(next_num, seq_len);
-        seq_len
-    }
-
-
-}
-
-pub fn problem_014() -> u64 {
-    let n = 1000000;
-    let mut seq_len: HashMap<u64,u64> = HashMap::new();
-    
-    // insert powers of two
-    for i in 0..20 {
-        seq_len.insert((2 as u64).pow(i),(i+1) as u64);
-    }
-    let mut max_seq = 0;
-    let mut arg_max = 0;
-    for i in 2..(n+1) {
-        let mut curr: u64 = i;
-        let mut count: u64 = 0;
-        while !seq_len.contains_key(&curr) {
-            curr = next_collatz(curr);
-            count += 1;
-        }
-
-        let seq_len_i = count + seq_len.get(&curr).unwrap();
-        seq_len.insert(i,seq_len_i);
-        if seq_len_i > max_seq {
-            max_seq = seq_len_i;
-            arg_max = i; 
-        }
-    }
-    arg_max
-}
-
-pub fn problem_014_b() -> u64 {
-    let n = 1000000;
-    let mut seq_len: HashMap<u64,u64> = HashMap::new();
-    
-    // insert powers of two
-    for i in 0..20 {
-        seq_len.insert((2 as u64).pow(i),(i+1) as u64);
-    }
-    let mut max_seq = 0;
-    let mut arg_max = 0;
-    for i in 2..10000{//(n+1) {
-
-        let seq_len_i = collatz_seq_length(i as u64, seq_len.clone());
-        seq_len.insert(i,seq_len_i);
-        if seq_len_i > max_seq {
-            max_seq = seq_len_i;
-            arg_max = i; 
-        }
-    }
-    println!("seq_len : {:?}",seq_len);
-    arg_max
-}
-
-pub fn problem_014_c() -> u64 {
+pub fn problem_014() -> usize {
     let n = 1000000;
     let mut collatz: Vec<usize> = vec![0;(n+1)];
 
@@ -86,11 +20,11 @@ pub fn problem_014_c() -> u64 {
                 collatz[i] += collatz[x];
                 if collatz[i] > max_seq {
                     max_seq = collatz[i];
-                    arg_max = i as u64; 
+                    arg_max = i; 
                 }
                 break; 
             } else {
-                x = next_collatz(x as u64) as usize;
+                x = next_collatz(x);
                 collatz[i] += 1;
             }
         }
@@ -106,7 +40,7 @@ mod test {
 
     #[test]
     fn test_problem_014() {
-        let ans: u64 = problem_014_c();
+        let ans: usize = problem_014();
         println!("Answer to Problem 14: {}", ans);
         assert!(ans == 837799)
     }
---
+++ problems/src/problem_015.rs
@@ -0,0 +1,41 @@
+
+
+pub fn problem_015() -> u64 {
+    let n = 20;
+
+    let mut num_paths: Vec<Vec<u64>> = vec![vec![0; (n+1)];(n+1)];
+
+    num_paths[0] = vec![1; (n+1)];
+    for i in 0..(n+1) {
+        num_paths[i][0] = 1;
+    }
+
+    for i in 1..(n+1){
+        for j in 1 .. (n + 1){
+            let from_left = num_paths[i][j - 1];
+            let from_above = num_paths[i - 1][j];
+            num_paths[i][j] = from_left + from_above;
+        }
+    }
+    num_paths[n][n]
+
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use test::Bencher;
+
+    #[test]
+    fn test_problem_015() {
+        let ans: u64 = problem_015();
+        println!("Answer to Problem 15: {}", ans);
+        assert!(ans == 137846528820)
+    }
+
+    #[bench]
+    fn bench_problem_015(b: &mut Bencher) {
+        b.iter(|| problem_015());
+    }
+}
