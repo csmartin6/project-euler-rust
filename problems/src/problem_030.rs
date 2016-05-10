4e168101b5772e7679a208e83fd94ebbfae65b6d
Chris Martin <cmartin@palantir.com>
2016-05-10 09:07:48 -0500

    Problem 30

---
+++ problems/src/problem_030.rs
@@ -0,0 +1,36 @@
+use utils;
+
+pub fn problem_030() -> usize {
+
+    let mut numbers: Vec<u32> = vec![];
+    for num in 2..250000 {
+        let digits = utils::to_digit_array(num);
+        let sum_of_fifth_powers = digits.iter().fold(0,|a,&b| a + b.pow(5));
+
+        if sum_of_fifth_powers == num as u32{
+            numbers.push(num as u32)
+        }
+    }
+    numbers.iter().fold(0,|a,&b| a + b as usize)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use test::Bencher;
+
+    #[test]
+    fn test_problem_030() {
+        let ans: usize = problem_030();
+        println!("Answer to Problem 30: {}", ans);
+        assert!(ans == 443839)
+    }
+
+    #[bench]
+    fn bench_problem_030(b: &mut Bencher) {
+        b.iter(|| problem_030());
+    }
+
+}
+
+
--- src/main.rs
+++ src/main.rs
@@ -64,6 +64,7 @@ fn main(){
             "problem_027" => run_problem(problems::problem_027::problem_027, arg),  
             "problem_028" => run_problem(problems::problem_028::problem_028, arg),  
             "problem_029" => run_problem(problems::problem_029::problem_029, arg),  
+            "problem_030" => run_problem(problems::problem_030::problem_030, arg),  
             "problem_148" => run_problem(problems::problem_148::problem_148, arg),             
     		_ => println!("Haven't done {} yet",arg)
 
