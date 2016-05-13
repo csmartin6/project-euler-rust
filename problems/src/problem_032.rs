56dfceb97d9a14037888243866ef855ccb6b7d66
Chris Martin <cmartin@palantir.com>
2016-05-12 21:32:41 -0500

    performance improvements for problem 32

--- problems/src/problem_032.rs
+++ problems/src/problem_032.rs
@@ -1,6 +1,7 @@
 use std::collections::HashSet;
 use utils;
 
+
 pub fn problem_032() -> u32 {
     // 2 possible multipulcation setups
     // 2 * 3 * 4
@@ -12,59 +13,50 @@ pub fn problem_032() -> u32 {
     let zero: u32 = 0;
 
     for a in 12..99{
-        let mut a_digits:  HashSet<u32> = HashSet::new();
-        for &d in utils::to_digit_array(a).iter(){
-            a_digits.insert(d);
-        }
-        if a_digits.len() == 2 {
-            for b in 123..988{
-                let mut digits: HashSet<u32> = HashSet::new();
-                for &d in a_digits.iter() {
+        for b in 123..988{
+            let m = a * b;
+            if m < 10000 {
+                let mut digits : HashSet<u32> = HashSet::new();
+                for &d in utils::to_digit_array(a).iter() {
                     digits.insert(d);
                 }
-                for &d in utils::to_digit_array(b).iter(){
+                for &d in utils::to_digit_array(b).iter() {
                     digits.insert(d);
                 }
-                if digits.len() == 5{
-                    let m = a * b;
-                    for &d in utils::to_digit_array(m).iter() {
-                        digits.insert(d);
-                    }
-                    if !(digits.contains(&zero)) & (digits.len() == 9) & (m < 10000){
-                        pandigital.insert(m as u32);
-                    }
-                }
-
-            }
-        }
-    }
-
-    for a in 1..10{
-        let mut a_digits: HashSet<u32> = HashSet::new();
-        for &d in utils::to_digit_array(a).iter(){
-            a_digits.insert(d);
-        }
-        for b in 1234..9877{
-            let mut digits: HashSet<u32> = HashSet::new();
-            for &d in a_digits.iter() {
-                digits.insert(d);
-            }
-            for d in utils::to_digit_array(b) {
-                digits.insert(d);
-            }
-            if digits.len() == 5{
-                let m = a * b;
-                for d in utils::to_digit_array(m) {
+                for &d in utils::to_digit_array(m).iter() {
                     digits.insert(d);
                 }
-                if !(digits.contains(&zero)) & (digits.len() == 9) & (m < 10000){
+                if !(digits.contains(&zero)) & (digits.len() == 9){
                     pandigital.insert(m as u32);
                 }
             }
         }
     }
+    for a in 2..10 {
+        for b in 1234..5000 {
+            let m = a * b;
+            if m < 10000 {
+                let mut digits : HashSet<u32> = HashSet::new();
+                
+                for &d in utils::to_digit_array(a).iter() {
+                    digits.insert(d);
+                }
+                for &d in utils::to_digit_array(b).iter() {
+                    digits.insert(d);
+                }
+                for &d in utils::to_digit_array(m).iter() {
+                    digits.insert(d);
+                }
+                if !(digits.contains(&zero)) & (digits.len() == 9){
+                    pandigital.insert(m as u32);
+                }
+            }
+            
+        }
+    }
     pandigital.iter().fold(0,|a,&b| a + b)
 }
+
 
 #[cfg(test)]
 mod test {
@@ -82,7 +74,6 @@ mod test {
     fn bench_problem_032(b: &mut Bencher) {
         b.iter(|| problem_032());
     }
-
 }
 
 
